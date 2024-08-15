b7f8fa46bcd547f0d399580d2f1bb1bd06bde5ca
Matveev Stepan Andreevich <mset321@gmail.com>
2024-08-15 21:55:54 +0300

    добавил auth

--- src/main.cpp
+++ src/main.cpp
@@ -9,38 +9,54 @@
 #include "include/user.h"
 
 
+static bool userVerify(crow::request req, db::DB& db) {
+    std::string auth = req.get_header_value("Authorization");
+    if (auth.empty()) {
+        return false;
+    }
 
+    std::string creds = auth.substr(6);
+    std::string d_creds = crow::utility::base64decode(creds, creds.size()); 
+    size_t f = d_creds.find(':');
+
+    if (f == std::string::npos || d_creds.find('\'') != std::string::npos) {
+        return false;
+    }
+
+    std::string uname = d_creds.substr(0, f);
+    std::string pword = d_creds.substr(f + 1);
+    return db.isUserExists(user::User(uname, pword));
+}
 
 int main() {
     SetConsoleOutputCP(CP_UTF8); 
     
-    // db::DB db("dbname=cpptest user=postgres password=1568");
+    db::DB db("dbname=cpptest user=postgres password=1568");
 
     crow::SimpleApp app;
 
+    CROW_ROUTE(app, "/")([&db](const crow::request& req) {
+        std::string aut1 = req.get_header_value("Authorization");
+        if (!aut1.empty()) {
+            std::string creds = aut1.substr(6);
+            std::string d_creds = crow::utility::base64decode(creds, creds.size()); 
+        }
 
-    CROW_ROUTE(app, "/") ([]() {
-        crow::response res;
-        res.set_static_file_info_unsafe("D:/testWebProd/src/statics/index.html");
-        return res;
-    });
 
-    
+        bool auth = userVerify(req, db);
+        if (!auth) {
+            crow::response res;
+            res.code = 401;
+            res.set_header("WWW-Authenticate", "Basic realm=\"User Visible Realm\"");
+            res.write("<html><body><h1>Authentication Required</h1></body></html>");
+            return res;
+        }
+        return crow::response("<html><body><h1>Welcome!</h1><p>You are successfully authenticated.</p></body></html>");
+    }); 
 
-    CROW_ROUTE(app, "/style.css") ([]() {
-        crow::response res;
-        res.set_static_file_info_unsafe("D:/testWebProd/src/statics/style.css");
-        return res;
-    });
-
-        
-
-    CROW_ROUTE(app, "/script.js") ([]() {
-        crow::response res;
-        res.set_static_file_info_unsafe("D:/testWebProd/src/statics/script.js");
-        return res;
-    });
-
+    for (const auto& u : db.getUsers()) {
+        std::cout << u << '\n';
+    }
 
     app.bindaddr("192.168.0.102")
     .port(8000)
